c6f8ad0230a89bc56b757cec382b096c1537aebe
rmdyh <rmdyh@hotmail.com>
2023-07-04 18:58:36 +0800

    modify the base url

--- vite.config.ts
+++ vite.config.ts
@@ -4,12 +4,14 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  },
-  // base: 'https://thoughost.github.io'
+export default defineConfig(({command, mode}) => {
+  return {
+    plugins: [vue()],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
+      }
+    },
+    base: 'https://thoughost.github.io/'
+  }
 })
