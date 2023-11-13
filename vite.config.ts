3912693f875d507920331e05445a53785075e7a2
wheatfox <enkerewpo@hotmail.com>
2023-11-13 10:42:19 +0800

    add image size optimization plugin

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,38 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import viteImagemin from 'vite-plugin-imagemin'
 
 // https://vitejs.dev/config/
-export default defineConfig(({command, mode}) => {
+export default defineConfig(({ command, mode }) => {
   return {
-    plugins: [vue()],
+    plugins: [
+      vue(),
+      viteImagemin({
+        // https://github.com/vbenjs/vite-plugin-imagemin
+        optipng: {
+          optimizationLevel: 7,
+        },
+        mozjpeg: {
+          quality: 35,
+        },
+        pngquant: {
+          quality: [0.8, 0.9],
+          speed: 4,
+        },
+        svgo: {
+          plugins: [
+            {
+              name: 'removeViewBox',
+            },
+            {
+              name: 'removeEmptyAttrs',
+              active: false,
+            },
+          ],
+        },
+      }),
+    ],
     resolve: {
       alias: {
         '@': fileURLToPath(new URL('./src', import.meta.url))
