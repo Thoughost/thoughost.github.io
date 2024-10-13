1fb78d0c401d71bd19f5b8fa8ef35928248ae59f
enkerewpo <enkerewpo@users.noreply.github.com>
2024-10-13 10:04:56 +0000

    Deploying to gh-pages from  @ 7ec6893fc761017bc2bceed6f6e6673dce655bc7 🚀

---
+++ assets/BlackholeView.7cce6762.js
@@ -0,0 +1,10 @@
+import{i,_,o as s,c as l,b as e}from"./index.4a1e641b.js";const o=i({created(){window.addEventListener("resize",this.resize_handle),window.innerWidth<768&&(this.ifViewOnMobile=!0)},destroyed(){window.removeEventListener("resize",this.resize_handle)},computed:{},methods:{resize_handle(n){n.target.innerWidth<768?this.ifViewOnMobile=!0:this.ifViewOnMobile=!1}},data(){return{ifViewOnMobile:!1}}}),r={class:"container"};function a(n,t,d,c,w,f){return s(),l("div",r,t[0]||(t[0]=[e("div",{class:"page-title"},[e("h2",null,"BLACKHOLE")],-1),e("br",null,null,-1),e("div",{style:{"align-items":"center",display:"flex","justify-content":"center"}},[e("pre",null,`
+ ________________________________
+/ webpage under construction !!! \\
+ --------------------------------
+        \\   ^__^
+         \\  (oo)\\_______
+            (__)\\       )\\/\\
+                ||----w |
+                ||     ||
+    `)],-1),e("br",null,null,-1),e("div",{style:{"align-items":"center",display:"flex","justify-content":"center"}},[e("img",{src:"https://www.nasa.gov/wp-content/uploads/2019/09/bh_labeled.jpg",alt:"blackhole",style:{width:"100%"}})],-1)]))}const p=_(o,[["render",a]]);export{p as default};
